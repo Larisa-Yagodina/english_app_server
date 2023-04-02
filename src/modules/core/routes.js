8c5d7560a33fa637d1e6c7853396b4b7f3e2b3f4
larisayagodina <cheeseroll@mail.ru>
2023-04-02 16:48:24 +0300

    favicon

--- src/modules/core/routes.js
+++ src/modules/core/routes.js
@@ -5,6 +5,7 @@ import servicesRouter from '../services/ServicesRouter';
 import UserPhrasesRouter from '../userPhrases/UserPhrasesRouter';
 import userRouter from '../users/userRouter';
 
+// english-app-server-7edr2daej-larisa-yagodina.vercel.app
 
 export default function routes(app){
   app.use('/user', userRouter)
@@ -12,7 +13,6 @@ export default function routes(app){
   app.use('/statuses', statusesRouter)
   app.use('/services', servicesRouter)
   app.use('/userPhrases', UserPhrasesRouter)
-  app.get('/favicon.ico', (req, res) => res.status(204));
-  app.use('/', home)
+
 }
 
