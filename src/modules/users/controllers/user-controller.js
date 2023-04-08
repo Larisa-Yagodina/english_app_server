9213db5a60192a69e525618e6e77d542de995d64
larisayagodina <cheeseroll@mail.ru>
2023-04-08 15:15:13 +0300

    reg & login res correct

--- src/modules/users/controllers/user-controller.js
+++ src/modules/users/controllers/user-controller.js
@@ -5,16 +5,35 @@ import UserModel from '../Model';
 import bcrypt from 'bcrypt';
 import UserDto from '../dtos/userDto';
 import tokenService from '../services/TokenService';
+import * as uuid from 'uuid';
+import mailService from '../services/MailService';
 
 class UserController {
   async registration(req, res, next) {
     try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
-        return next(ApiError.BadRequest('Ошибка при валидации', errors.array()));
+        //return next(ApiError.BadRequest('Ошибка при валидации', errors.array()));
+        res.status(402).send('Пожалуйста, введите корректные данные')
       }
       const {email, password} = req.body;
-      const userData = await UserService.registration(email, password);
+      //const userData = await UserService.registration(email, password);
+
+      const candidate = await UserModel.findOne({email});
+      if (candidate) {
+        //throw ApiError.BadRequest(`Пользователь с адресом ${email} уже существует`);
+        res.status(402).send(`Пользователь с адресом ${email} уже существует`)
+      }
+      const hashPassword = await bcrypt.hash(password, 3);
+      const activationLink = uuid.v4();
+      const user = await UserModel.create({email, password: hashPassword, activationLink, isActivated: false});
+      await mailService.sendActivationMail(email, `${process.env.API_URL}/user/activation/${activationLink}`);
+      const userDto = new UserDto(user);
+      const tokens = tokenService.generateTokens({...userDto});
+      await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+      const userData = {...tokens, user: userDto};
+
       res.cookie('refreshToken', userData.refreshToken, {maxAge: 3 * 24 * 60 * 60 * 1000, httpOnly: true, secure: true, sameSite: 'none'});
 
       return res.json(userData);
