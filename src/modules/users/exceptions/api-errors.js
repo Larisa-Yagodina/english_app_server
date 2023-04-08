68f7e8b2e7940ccc0549aa555b181b0c7ab1929e
larisayagodina <cheeseroll@mail.ru>
2023-04-08 14:54:10 +0300

    ApiError message

--- src/modules/users/exceptions/api-errors.js
+++ src/modules/users/exceptions/api-errors.js
@@ -1,13 +1,11 @@
 module.exports = class ApiError extends Error {
   status;
   errors;
-  name;
 
   constructor(status, message, errors = []) {
     super(message);
     this.status = status;
     this.errors = errors;
-    this.name = message;
   }
 
   static UnauthorizedError() {
